7f4b49dc02f3f47ae23d3d96ab66b238bd4c7305
Mokshini Baglekar <baglekarmokshini03@gmail.com>
2025-01-08 11:36:11 +0530

    add seventh program

---
+++ prog7.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class prog7 {
+	public static void main (String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int length = sc.nextInt();
+		int width = sc.nextInt();
+		
+		System.out.println(2 * (length + width));
+	}
+}
