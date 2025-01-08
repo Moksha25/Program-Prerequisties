efac90b92657e3e8d153cc428227aab13788aa65
Mokshini Baglekar <baglekarmokshini03@gmail.com>
2025-01-08 11:37:14 +0530

    add ninth program

---
+++ prog9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class prog2 {
+	public static void main (String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		double x=sc.nextDouble();
+		double y=sc.nextDouble();
+		double z=sc.nextDouble();
+		
+		System.out.println((x+y+z)/2);
+	}
+}
