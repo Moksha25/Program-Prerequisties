a81c8807a6951ed88b99bb3d736e6b858c696408
Mokshini Baglekar <baglekarmokshini03@gmail.com>
2025-01-08 11:22:27 +0530

    add third program

---
+++ prog3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class prog3 {
+	public static void main (String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		double Celsius = sc.nextDouble();
+		
+		System.out.println((Celsius * 9/5) + 32);
+	}
+}
